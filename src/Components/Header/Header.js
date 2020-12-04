22c04b6784ee6585d45b430dd0fab1e2de581ba0
asrafulnirob1376 <asrafulsharkernirob1376@gmail.com>
2020-12-04 20:11:00 +0600

    add navbar

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import './Header.css';
+import {
+    Navbar,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+} from 'reactstrap';
+import Logo from '../../assets/logo.png';
+
 
 const Header =()=>{
     return(
-        <div>
-            <p>Header</p>
+        <div className="Navigation">
+            <Navbar style={{
+                backgroundColor: "#D70F64",
+                height:"70px",
+            }}>
+                <NavbarBrand href="/" className="mr-auto ml-md-5 Brand">
+                    <img src={Logo} alt="Burger" width="60px"/>
+                </NavbarBrand>
+                <Nav className="mr-md-5">
+                    <NavItem>
+                        <NavLink href="/" className="NavLink">Something</NavLink>
+                    </NavItem>
+                </Nav>
+            </Navbar>
         </div>
     )
 }

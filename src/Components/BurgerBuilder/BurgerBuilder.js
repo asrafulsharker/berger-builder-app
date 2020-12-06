e98e53a18d1f2f7df543cae61694ce9b059c7d1c
asrafulnirob1376 <asrafulsharkernirob1376@gmail.com>
2020-12-06 20:53:03 +0600

    add slice

--- src/Components/BurgerBuilder/BurgerBuilder.js
+++ src/Components/BurgerBuilder/BurgerBuilder.js
@@ -1,27 +1,79 @@
 import React , {Component} from 'react';
 import Burger from './Burger/Burger';
 import Controls from './Controls/Controls';
+import {Modal,ModalBody,ModalHeader,ModalFooter,Button} from 'reactstrap';
+
+const INGREDIENT_PRICES ={
+    salad: 20,
+    cheese:40,
+    meat: 90,
+}
+
+
 export default class BurgerBuilder extends Component {
     state={
         ingredients:[
             {type: 'salad', amount: 0},
             {type: 'cheese', amount: 0},
-            {type: 'meat', amount: 4},
-        ]
+            {type: 'meat', amount: 0},
+        ],
+        totalPrice:80,
+        modalOpen:false,
     }
 
     addIngredientHandle = type=>{
-        console.log(type);
+        const ingredients = [...this.state.ingredients];
+        const newPrice = this.state.totalPrice + INGREDIENT_PRICES[type ];
+        for ( let item of ingredients){
+            if(item.type === type) item.amount++;
+        }
+        this.setState({ingredients: ingredients, totalPrice : newPrice});
     }
+
+    removeIngredientHandle =type=>{
+        const ingredients = [...this.state.ingredients];
+        const newPrice = this.state.totalPrice - INGREDIENT_PRICES[type ];
+        for ( let item of ingredients){
+            if(item.type === type){
+                if(item.amount <= 0)return;
+                item.amount --;
+            } 
+        }
+        this.setState({ingredients: ingredients, totalPrice : newPrice});
+    }
+
+    toggleModal=()=>{
+        this.setState({
+            modalOpen: !this.state.modalOpen
+        })
+    }
+
 
     render(){
         return(
-            <div className="d-flex flex-md-row flex-column">
+            <div>
+           <div className="d-flex flex-md-row flex-column">
                 <Burger ingredients={this.state.ingredients}/>
                 <Controls
                     ingredientAdded = {this.addIngredientHandle}
+                    ingredientRemoved = {this.removeIngredientHandle}
+                    price={this.state.totalPrice}
+                    toggleModal={this.toggleModal}
                 />
             </div>
+                <Modal isOpen={this.state.modalOpen}>
+                    <ModalHeader>Your Order Summary</ModalHeader>
+                    <ModalBody>
+                        <h5>Total Price: {this.state.totalPrice.toFixed(0)} BDT</h5>
+                        
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button color="success"onClick={this.toggleModal}>Continue to Checkout</Button>
+                        <Button color="secondary" onClick={this.toggleModal}>Cancel</Button>
+                    </ModalFooter>
+                </Modal>
+            </div>
+ 
         )
     }
 }
--- src/Components/BurgerBuilder/Controls/Controls.js
+++ src/Components/BurgerBuilder/Controls/Controls.js
@@ -11,7 +11,7 @@ const BuildControl = props =>{
     return (
         <div className="d-flex">
             <div className="mr-auto ml-5" style={{fontWeight: "bold",fontSize:"1.2rem"}}>{props.label}</div>
-            <button className="btn btn-danger btn-sm m-1">Less</button>
+            <button className="btn btn-danger btn-sm m-1" onClick={props.removed}>Less</button>
             <button className="btn btn-success btn-sm m-1" onClick={props.added}>More</button>
         </div>
         )
@@ -37,11 +37,13 @@ const Controls = props =>{
                              type={item.type} 
                              key={Math.random()}
                              added={()=>props.ingredientAdded(item.type)}
+                             removed={()=>props.ingredientRemoved(item.type)}
                              />
                         })
                     }
                 </CardBody>
-                <CardFooter><h5>Price: BDT</h5></CardFooter>
+                <CardFooter><h5>Price: <strong>{props.price}</strong> BDT</h5></CardFooter>
+                <Button>Order Now</Button>
             </Card>
         </div>
     )
